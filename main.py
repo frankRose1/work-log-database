3b265de18a0861d036c654eae3285619d5467a22
Frank Rosendorf <frank.rosendorf1@gmail.com>
2018-12-30 14:10:31 -0500

    Add create task and delete task functions

--- main.py
+++ main.py
@@ -17,8 +17,8 @@ class BaseClass(Model):
         database = db
 
 class Task(BaseClass):
-    employee_name = CharField(max_length=80)
-    title = CharField(max_length=150)
+    employee= CharField(max_length=150)
+    title = CharField(max_length=200)
     date = DateField()
     notes = TextField()
 
@@ -30,7 +30,13 @@ def initialize_db():
 
 def add_task():
     """Create a new task entry"""
-    pass
+    employee_name = input('Employee name: ').strip()
+    task_title = input('Title: ').strip()
+    task_date = get_date_input()
+    task_notes = input('Notes: ').strip()
+    if input('Save entry? [Yn] ').lower().strip() != 'n':
+        Task.create(employee=employee_name, title=task_title, date=task_date, notes=task_notes)
+        print('Entry saved!')
 
 def search_tasks():
     """Search tasks"""
@@ -39,6 +45,24 @@ def search_tasks():
 def quit_program():
     """Quit program"""
     print('\nUser has ended the program.')
+
+def get_date_input():
+    """Get task date from the user"""
+    while True:
+        date_str = input('Task date, use DD/MM/YYYY format: ').strip()
+        try:
+            date = datetime.datetime.strptime(date_str, '%d/%m/%Y')
+            break
+            return date
+        except ValueError:
+            print('Error: {} doesn\'t seem to be a valid date, please try again.'.format(date_str))
+
+def delete_task(task):
+    """Deletes a task from the Database"""
+    if input('Are your sure? [yN] ').lower().strip() == 'y':
+        task.delete_instance()
+        print('Task was deleted!')
+
 
 menu = OrderedDict([
     ('a', add_task),
