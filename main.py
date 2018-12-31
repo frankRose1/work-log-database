486040b7d9c42c386a9605814fcdcb1b4ccda9dc
Frank Rosendorf <frank.rosendorf1@gmail.com>
2018-12-30 19:15:06 -0500

    Add search menu and way to page through tasks

--- main.py
+++ main.py
@@ -19,8 +19,9 @@ class BaseClass(Model):
 class Task(BaseClass):
     employee= CharField(max_length=150)
     title = CharField(max_length=200)
+    time_spent = IntegerField()
     date = DateField()
-    notes = TextField()
+    notes = TextField(null=True)
 
 
 def initialize_db():
@@ -28,23 +29,39 @@ def initialize_db():
     db.connect()
     db.create_tables([Task], safe=True)
 
+
+def search_tasks():
+    """Search tasks"""
+    tasks = Task.select()
+    for task in tasks:
+        print('='*20)
+        print(task.title)
+        print(task.employee)
+        print(task.date)
+        if task.notes:
+            print(task.notes)
+        print('\n'+'='*20)
+        print('[N]ext, [E]dit, [D]elete [R]eturn to menu')
+        next_action = input('Action: [Nedr] ').strip().lower()
+
+        if next_action == 'r':
+            break
+        elif next_action == 'd':
+            delete_task(task)
+        elif next_action == 'e':
+            edit_task(task)
+
 def add_task():
     """Create a new task entry"""
     employee_name = input('Employee name: ').strip()
     task_title = input('Title: ').strip()
+    task_time = get_time_input()
     task_date = get_date_input()
-    task_notes = input('Notes: ').strip()
+    task_notes = input('Notes (optional): ').strip()
     if input('Save entry? [Yn] ').lower().strip() != 'n':
-        Task.create(employee=employee_name, title=task_title, date=task_date, notes=task_notes)
+        Task.create(employee=employee_name, title=task_title, 
+                    time_spent=task_time, date=task_date, notes=task_notes)
         print('Entry saved!')
-
-def search_tasks():
-    """Search tasks"""
-    pass
-
-def quit_program():
-    """Quit program"""
-    print('\nUser has ended the program.')
 
 def get_date_input():
     """Get task date from the user"""
@@ -52,10 +69,19 @@ def get_date_input():
         date_str = input('Task date, use DD/MM/YYYY format: ').strip()
         try:
             date = datetime.datetime.strptime(date_str, '%d/%m/%Y')
-            break
             return date
         except ValueError:
             print('Error: {} doesn\'t seem to be a valid date, please try again.'.format(date_str))
+
+def get_time_input():
+    """Gets the time spent on the task from the user"""
+    while True:
+        time_input = input('Time spent on task (in minutes): ')
+        try:
+            time_input = int(time_input) 
+            return time_input
+        except ValueError:
+            print('Error: "{}" doesn\'t seem to be a valid time. Numbers only!'.format(time_input))
 
 def delete_task(task):
     """Deletes a task from the Database"""
@@ -63,24 +89,66 @@ def delete_task(task):
         task.delete_instance()
         print('Task was deleted!')
 
+def edit_task(task):
+    """Updates/saves a task"""
+    print(task)
 
-menu = OrderedDict([
+def quit_program():
+    """Quit program"""
+    print('\nUser has ended the program.')
+
+def search_by_name():
+    """Search by employee name"""
+    pass
+
+def search_by_date():
+    """Search by date"""
+    pass
+
+def search_by_time():
+    """Search by time spent on task"""
+    pass
+
+def search_by_term():
+    """Search by a term in either title or notes"""
+    pass
+
+search_menu = OrderedDict([
+    ('a', search_by_name),
+    ('b', search_by_date),
+    ('c', search_by_time),
+    ('d', search_by_term)
+])
+
+def print_search_menu():
+    """Search tasks"""
+    search_choice = None
+    while search_choice != 'q':
+        print('Enter "q" to return to main menu')
+        for key, value in search_menu.items():
+            print('{}) {}'.format(key, value.__doc__))
+
+        search_choice = input('Your choice: ').lower().strip()
+        if search_choice in search_menu:
+            search_menu[search_choice]()
+
+main_menu = OrderedDict([
     ('a', add_task),
-    ('s', search_tasks),
+    ('s', print_search_menu),
     ('q', quit_program)
 ])
 
-def print_menu():
+def print_main_menu():
     """Print a menu of options for the user"""
     user_choice = None
     while user_choice != 'q':
-        for key, value in menu.items():
+        for key, value in main_menu.items():
             print('{}) {}'.format(key, value.__doc__))
         user_choice = input('Your choice: ').lower().strip()
-        if user_choice in menu:
-            menu[user_choice]()
+        if user_choice in main_menu:
+            main_menu[user_choice]()
 
 if __name__ == '__main__':
     """Initialize the db and print the menu"""
     initialize_db()
-    print_menu()
+    print_main_menu()
