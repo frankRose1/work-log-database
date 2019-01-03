4d7ad50d5f7b53cc8238804ae335847071e90dfe
Frank Rosendorf <frank.rosendorf1@gmail.com>
2019-01-03 15:37:00 -0500

    Finish tests for search class

--- readme.md
+++ readme.md
@@ -2,15 +2,24 @@
 
 Command line application that will allow users to enter their name, task worked on, the amount of time, and general notes about the task.
 Information will be stored in a Sqlite database and there are various means the search tasks. This is an improvement on a previous project that I did where
-tasks were saved to the file system in a csv file.
+tasks were saved to the file system in a csv file. Also includes unit tests.
 
 ## App Features
 
-- to start the app, in the command line run ```pipenv
+- to start the app, in the command line run `pipenv run python work_log.py`
 - Menu loop will prompt users for actions adding tasks, various search methods, and an option to quit the program
 - New tasks can be created and added to the database
 - Tasks can be searched by date, employee name, time_spent, or term in notes/title
 - Can page through search results and options are provided to edit or delete a specific task
+- unit tests can be found in tests.py
+
+## Tests
+
+- currently there is 65% coverage
+- tests are included for `search.py` and `validator.py`
+- to run tests:
+  - in the root directory run `pipenv run coverage run tests.py`
+  - for a detailed report run `pipenv run coverage report -m`
 
 ## Technologies Used
 
--- tests.py
+++ tests.py
@@ -1,5 +1,6 @@
 """ 
-	Tests for the functions in main.py
+	Unit tests for the application
+	Tests are included for Search, Validator, and work_log
 """
 import unittest
 import datetime
@@ -36,59 +37,84 @@ test_entry_2 = {
 @patch('builtins.input')
 class ValidatorTests(unittest.TestCase):
 
-	def test_validate_date(self, MockInput):
-		"""Test that a user can enter a valid date"""
-		user_input=['30/12/2018']
-		MockInput.side_effect = user_input
-		expected_input = Validator.validate_date()
-		self.assertEqual(expected_input, test_entry_1['date'])
+		def test_validate_date(self, MockInput):
+				"""Test that a user can enter a valid date"""
+				user_input=['30/12/2018']
+				MockInput.side_effect = user_input
+				expected_input = Validator.validate_date()
+				self.assertEqual(expected_input, test_entry_1['date'])
 
-	def test_validate_time(self, MockInput):
-		"""Test that a user can enter a time duration in minutes"""
-		user_input = ['120']
-		MockInput.side_effect = user_input
-		expected_input = Validator.validate_time()
-		self.assertEqual(expected_input, int(user_input[0]))
+		def test_validate_time(self, MockInput):
+				"""Test that a user can enter a time duration in minutes"""
+				user_input = ['120']
+				MockInput.side_effect = user_input
+				expected_input = Validator.validate_time()
+				self.assertEqual(expected_input, int(user_input[0]))
 
-	def test_validate_employee(self, MockInput):
-		"""Test that a user can enter a valid employee name"""
-		user_input = [test_entry_1['employee']]
-		MockInput.side_effect = user_input
-		expected_input = Validator.validate_employee()
-		self.assertEqual(expected_input, test_entry_1['employee'])
+		def test_validate_employee(self, MockInput):
+				"""Test that a user can enter a valid employee name"""
+				user_input = [test_entry_1['employee']]
+				MockInput.side_effect = user_input
+				expected_input = Validator.validate_employee()
+				self.assertEqual(expected_input, test_entry_1['employee'])
 
-	def test_validate_title(self, MockInput):
-		"""Test that a user can enter a valid task title"""
-		user_input = [test_entry_1['title']]
-		MockInput.side_effect = user_input
-		expected_input = Validator.validate_title()
-		self.assertEqual(expected_input, test_entry_1['title'])
+		def test_validate_title(self, MockInput):
+				"""Test that a user can enter a valid task title"""
+				user_input = [test_entry_1['title']]
+				MockInput.side_effect = user_input
+				expected_input = Validator.validate_title()
+				self.assertEqual(expected_input, test_entry_1['title'])
 
 class BaseTestCase(unittest.TestCase):
-	"""To be used on any tests that require the database being set up"""
-	def setUp(self):
-		test_db.bind(MODELS,bind_refs=False, bind_backrefs=False)
-		test_db.connect()
-		test_db.create_tables(MODELS)
-		self.task_1 = test_entry_1
-		self.task_2 = test_entry_2
-		work_log.add_task(self.task_1)
-		work_log.add_task(self.task_2)
-		
-	def tearDown(self):
-		test_db.drop_tables(MODELS)
-		test_db.close()
+		"""To be used on any tests that require the database being set up"""
+		def setUp(self):
+				test_db.bind(MODELS,bind_refs=False, bind_backrefs=False)
+				test_db.connect()
+				test_db.create_tables(MODELS)
+				self.task_1 = test_entry_1
+				self.task_2 = test_entry_2
+				self.add_test_tasks(self.task_1)
+				self.add_test_tasks(self.task_2)
+			
+		def tearDown(self):
+				test_db.drop_tables(MODELS)
+				test_db.close()
+
+		def add_test_tasks(self, test_task):
+				with patch('builtins.input', side_effect=['y']):
+						# will cause "Add Entry!" to be printed to the terminal
+						work_log.add_task(test_task)
+
 
 @patch('builtins.input')
 class SearchTests(BaseTestCase):
 
-	def test_search_by_time(self, MockInput):
-		"""Two results should be returned that match 45 minutes"""
-		user_input = [45]
-		MockInput.side_effect = user_input
-		expected_output = Search.by_time()
-		self.assertEqual(len(expected_output), 2)
+		def test_search_by_time(self, MockInput):
+				"""Two results should be returned that match 45 minutes"""
+				user_input = [45]
+				MockInput.side_effect = user_input
+				expected_output = Search.by_time()
+				self.assertEqual(len(expected_output), 2)
 
+		def test_search_by_employee(self, MockInput):
+				"""Should be one entry found for the employee John"""
+				MockInput.side_effect = ['john']
+				expected_output = Search.by_name()
+				self.assertEqual(len(expected_output), 1)
+				self.assertEqual(expected_output[0].employee, self.task_2['employee'])
+
+		def test_search_by_date(self, MockInput):
+				"""Should show one result for 30/12/2018"""
+				MockInput.side_effect = ['30/12/2018']
+				expected_output = Search.by_date()
+				self.assertEqual(len(expected_output), 1)
+				self.assertIsInstance(expected_output[0].employee, str)
+
+		def test_search_by_term(self, MockInput):
+				"""Should return 2 search results for the term "test" """
+				MockInput.side_effect = ['test']
+				expected_output = Search.by_term()
+				self.assertEqual(len(expected_output), 2)
 
 if __name__ == '__main__':
     unittest.main()
